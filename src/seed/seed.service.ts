cdc4639f04588ca34418907f55d869729d545590
fmel025 <00018720@uca.edu.sv>
2024-02-11 13:20:16 -0600

    feat: add seed feature for pokemons :sparkles:

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  executeSeed(){
-    return 'Seed executed';
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
+
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    const pokemonsToInsert: { name: string; no: number }[] = data.results.map(
+      ({ name, url }) => {
+        const segments = url.split('/');
+        const no = parseInt(segments[segments.length - 2]);
+        return { name, no };
+      },
+    );
+
+    await this.pokemonModel.insertMany(pokemonsToInsert);
+
+    return pokemonsToInsert;
   }
 }
