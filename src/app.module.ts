0a6b5f647c81a625d7ed7e69ac4d9d5695b25917
fmel025 <00018720@uca.edu.sv>
2024-02-11 20:25:55 -0600

    feat: add env config to config module :wrench:

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { EnvConfiguration } from './common/config/app.config';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     PokemonModule,
     CommonModule,
---
+++ src/common/config/app.config.ts
@@ -0,0 +1,6 @@
+export const EnvConfiguration = () => ({
+  environment: process.env.NODE_ENV || 'dev',
+  mongoUri: process.env.MONGO_URI,
+  port: process.env.PORT || 3000,
+  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+});
